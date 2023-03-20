131768d5e8d04c2c70de7082f6b6be65c2ea4f0b
CordurieL <82098372+CordurieL@users.noreply.github.com>
2023-03-20 10:15:15 +0100

    signin part1

--- app/auth/routes/auth.js
+++ app/auth/routes/auth.js
@@ -4,9 +4,8 @@ const knex = require("../db_connection");
 const Joi = require("joi");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const randToken = require("rand-token");
 
-/* Méthode POST sur /auth */
-// used to signup, must return access_token and refresh_token
 router.post("/signup", async (req, res, next) => {
   const schema = Joi.object({
     name: Joi.string().min(1).max(50).required(),
@@ -55,8 +54,121 @@ router.post("/signup", async (req, res, next) => {
   }
 });
 
-/* Méthode pour tout Uri invalide */
-router.all("/:id", async (req, res, next) => {
+router.post("/signin", async (req, res, next) => {
+  const schema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).required(),
+  });
+
+  const { error, value } = schema.validate(req.body);
+
+  if (!error) {
+    try {
+      // On récupère le mot de passe hashé de l'utilisateur
+      const hashedPassword = await knex("clients")
+        .select("password")
+        .where("email", value.email)
+        .first();
+
+      // On vérifie que l'utilisateur existe
+      if (hashedPassword) {
+        // On compare le mot de passe entré avec le mot de passe hashé
+        const validPassword = await bcrypt.compare(
+          value.password,
+          hashedPassword.password,
+          function (err, result) {
+            if (err) {
+              res.sendStatus(500);
+            }
+            return result;
+          }
+        );
+
+        // Si le mot de passe est correct, on génère un token JWT
+        if (validPassword) {
+          const token = await jwt.sign(
+            {
+              email: value.email,
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            {
+              expiresIn: "1h",
+            }
+          );
+
+          // Et un refresh token
+          const refreshToken = await randToken.generate(30);
+
+          res.status(200).json({
+            access_token: token,
+            refresh_token: refreshToken,
+          });
+        } else {
+          res.status(401).json({
+            type: "error",
+            error: 401,
+            message: "Mot de passe incorrect",
+          });
+        }
+      } else {
+        res.status(409).json({
+          type: "error",
+          error: 409,
+          message: "Cet utilisateur n'existe pas",
+        });
+      }
+    } catch (err) {
+      res.sendStatus(500);
+    }
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+router.post("/refreshToken", async (req, res, next) => {
+  const schema = Joi.object({
+    refresh_token: Joi.string().required(),
+  });
+
+  const { error, value } = schema.validate(req.body);
+
+  if (!error) {
+    try {
+      const refreshToken = await knex("refresh_tokens")
+        .select("refresh_token")
+        .where("refresh_token", value.refresh_token)
+        .first();
+
+      if (refreshToken) {
+        const token = await jwt.sign(
+          {
+            id: refreshToken.id,
+          },
+          process.env.ACCESS_TOKEN_SECRET,
+          {
+            expiresIn: "1h",
+          }
+        );
+
+        res.status(200).json({
+          access_token: token,
+        });
+      } else {
+        res.status(401).json({
+          type: "error",
+          error: 401,
+          message: "Token invalide",
+        });
+      }
+    } catch (err) {
+      res.sendStatus(500);
+    }
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+router.all("/", async (req, res, next) => {
   res.status(405).json({
     type: "error",
     error: 405,
