f5389be762e7fcc754a575c83041de4517d81edc
CordurieL <82098372+CordurieL@users.noreply.github.com>
2023-03-21 13:45:15 +0100

    singup lbs

--- app/lbs/routes/users.js
+++ app/lbs/routes/users.js
@@ -1,11 +1,30 @@
 const axios = require("axios");
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-// POST /users/{id}/signup
-// POST /users/signin
-// POST /users/signout
+router.post("/signup", async (req, res, next) => {
+  const schema = Joi.object({
+    name: Joi.string().min(1).max(50).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).required(),
+  });
+  const { error, value } = schema.validate(req.body);
 
+  if (!error) {
+    try {
+      const user = await axios.post("http://node_auth:3000/auth/signup", {
+        name: value.name,
+        email: value.email,
+        password: value.password,
+      });
 
+      return user.data;
+    } catch (err) {
+      res.sendStatus(500);
+    }
+  } else {
+    res.sendStatus(400);
+  }
+});
 
 module.exports = router;
